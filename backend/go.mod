74b2cbc6d45b453bc77c41c97bc919c7b3e3c656
Peijun Ma <peijun.ma@protonmail.com>
2023-03-05 23:30:20 -0500

    Cache invalid secrets

--- backend/go.mod
+++ backend/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/websocket v1.5.0
 	github.com/nicklaw5/helix v1.25.0
+	golang.org/x/exp v0.0.0-20230304125523-9ff063c70017
 	gotest.tools/v3 v3.4.0
 )
 
@@ -20,7 +21,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
-	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.2 // indirect
--- backend/slaytherelics/users.go
+++ backend/slaytherelics/users.go
@@ -3,18 +3,23 @@ package slaytherelics
 import (
 	"context"
 	"crypto/sha256"
+	"errors"
 	"fmt"
 	"strings"
 	"sync"
 
+	"golang.org/x/exp/slices"
+
 	"github.com/MaT1g3R/slaytherelics/client"
+	errors2 "github.com/MaT1g3R/slaytherelics/errors"
 )
 
 type Users struct {
 	twitch *client.Twitch
 
-	userIDCache   sync.Map
-	userAuthCache sync.Map
+	userIDCache    sync.Map
+	userAuthCache  sync.Map
+	invalidSecrets sync.Map
 }
 
 func NewUsers(twitch *client.Twitch) *Users {
@@ -44,6 +49,11 @@ func (s *Users) GetUserID(ctx context.Context, login string) (string, error) {
 func (s *Users) UserAuth(ctx context.Context, login string, secret string) (bool, error) {
 	secretHash := fmt.Sprintf("%x", sha256.Sum256([]byte(secret)))
 
+	invalidSecrets, _ := s.invalidSecrets.LoadOrStore(login, []string{})
+	if slices.Contains(invalidSecrets.([]string), secretHash) {
+		return false, nil
+	}
+
 	cacheResult, ok := s.userAuthCache.Load(login)
 	if ok {
 		gotSecretHash := cacheResult.(string)
@@ -51,10 +61,16 @@ func (s *Users) UserAuth(ctx context.Context, login string, secret string) (bool
 	}
 
 	user, err := s.twitch.VerifyUserName(ctx, login, secret)
+	authErr := &errors2.AuthError{}
+	if errors.As(err, &authErr) {
+		s.invalidSecrets.Store(login, append(invalidSecrets.([]string), secretHash))
+	}
+
 	if err != nil {
 		return false, err
 	}
 	if !strings.EqualFold(user, login) {
+		s.invalidSecrets.Store(login, append(invalidSecrets.([]string), secretHash))
 		return false, nil
 	}
 
