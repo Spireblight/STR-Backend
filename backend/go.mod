4920f6e7a152142df30db91d2c99f5f36621ce21
Peijun Ma <peijun.ma@protonmail.com>
2023-03-10 13:27:37 -0500

    Use uptrace

--- backend/go.mod
+++ backend/go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/websocket v1.5.0
 	github.com/nicklaw5/helix v1.25.0
+	github.com/uptrace/uptrace-go v1.13.0
 	go.opentelemetry.io/otel v1.14.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.14.0
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.14.0
@@ -32,7 +33,7 @@ require (
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.2 // indirect
@@ -42,15 +43,20 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.10 // indirect
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.39.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.14.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric v0.36.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.36.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.14.0 // indirect
+	go.opentelemetry.io/otel/metric v0.36.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v0.36.0 // indirect
 	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
 	golang.org/x/arch v0.2.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
-	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	google.golang.org/genproto v0.0.0-20230202175211-008b39050e57 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- backend/o11y/o11y.go
+++ backend/o11y/o11y.go
@@ -3,24 +3,18 @@ package o11y
 import (
 	"context"
 	"errors"
-	"fmt"
-	"os"
-	"time"
 
+	"github.com/uptrace/uptrace-go/uptrace"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/metric/global"
 	"go.opentelemetry.io/otel/trace"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 var Tracer trace.Tracer
+var Meter metric.Meter
 
 func End(span *trace.Span, err *error) {
 	defer (*span).End()
@@ -51,66 +45,13 @@ func End(span *trace.Span, err *error) {
 	}
 }
 
-func Init(ctx context.Context, serviceName, otelEndpoint string) (func(), error) {
-	shutdown := func() {}
-
-	exp, err := newExporter(ctx, otelEndpoint)
-	if err != nil {
-		return shutdown, err
-	}
-
-	tp, err := newTraceProvider(exp, serviceName)
-	if err != nil {
-		return shutdown, err
-	}
-
-	shutdown = func() { _ = tp.Shutdown(ctx) }
-	otel.SetTracerProvider(tp)
-
-	Tracer = tp.Tracer(serviceName)
-	return shutdown, nil
-}
-
-func newExporter(ctx context.Context, otelEndpoint string) (sdktrace.SpanExporter, error) {
-	if otelEndpoint == "" {
-		return stdouttrace.New(
-			stdouttrace.WithWriter(os.Stderr),
-		)
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, time.Second)
-	defer cancel()
-
-	conn, err := grpc.DialContext(ctx, otelEndpoint,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithBlock(),
+func Init(serviceName string) func(context.Context) {
+	uptrace.ConfigureOpentelemetry(
+		uptrace.WithServiceName(serviceName),
 	)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
-	}
-	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
-	if err != nil {
-		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
-	}
-	return traceExporter, nil
-}
+	shutdown := func(ctx context.Context) { _ = uptrace.Shutdown(ctx) }
+	Tracer = otel.Tracer(serviceName)
+	Meter = global.Meter(serviceName)
 
-func newTraceProvider(exp sdktrace.SpanExporter, serviceName string) (*sdktrace.TracerProvider, error) {
-	// Ensure default SDK resources and the required service name are set.
-	r, err := resource.Merge(
-		resource.Default(),
-		resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceName(serviceName),
-		),
-	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(exp),
-		sdktrace.WithResource(r),
-	), nil
+	return shutdown
 }
