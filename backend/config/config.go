807452ae08b8e04850280e2bb498d7b6f19a3ca6
Zach Dylag <zdylag@gmail.com>
2024-10-09 18:46:05 -0700

    go backend: add pprof endpoints for debugging
    This should allow us to easily get profiles in the future when any
    unwanted behavior is occuring.
    
    This should be safe to add as it is registered on to a separate port
    from any of the existing actual APIs or metrics.

--- backend/config/config.go
+++ backend/config/config.go
@@ -4,6 +4,7 @@ import "github.com/alecthomas/kong"
 
 type Config struct {
 	ListenAddr      string `env:"LISTEN_ADDR" default:":8080"`
+	PprofAddr       string `env:"PPROF_ADDR" default:":8081"`
 	ClientID        string `env:"CLIENT_ID"`
 	ClientSecret    string `env:"CLIENT_SECRET"`
 	OwnerUserID     string `env:"OWNER_USER_ID"`
