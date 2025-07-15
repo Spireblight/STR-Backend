a6716b529aa0a6a50b1b1a52d9b70e974da4f621
Peijun Ma <peijun.ma@protonmail.com>
2025-07-15 13:53:04 -0400

    Use react-tooltip

--- slay-the-relics-extension/src/components/Tip/Tip.tsx
+++ slay-the-relics-extension/src/components/Tip/Tip.tsx
@@ -1,4 +1,5 @@
-import { CSSProperties, ReactNode, useState } from "react";
+import { CSSProperties, ReactNode, useId } from "react";
+import { Tooltip } from "react-tooltip";
 
 export type HitBox = {
   x: string;
@@ -17,6 +18,15 @@ const MAX_POWERTIP_MULTICOL_HEIGHT = 70.0; //%
 const POWERTIP_BOTTOM_MARGIN = 0.365; //%
 const MULTICOL_COLUMN_RIGHT_MARGIN = 0.469; //% - don't mess with this number or the columns won't be separated
 
+const MAX_DISPLAY_RELICS = 25; //count
+
+const MAX_RIGHT = 99.0; //%
+const MAX_BOTTOM = 98.0; //%
+const MIN_TOP = 2.0; //%
+const CHARACTER_POWERS_OFFSET_R = 1.0416; //%
+const CHARACTER_POWERS_OFFSET_L = -2.917; //%
+const CHARACTER_HEALTHBAR_HEIGHT = 6.666; //%
+
 export class Tip {
   header: string;
   description: string;
@@ -29,19 +39,12 @@ export class Tip {
   }
 }
 
-export function PowerTip(props: {
-  tip: Tip;
-  offset: { x: string; y: string };
-}) {
+export function PowerTip(props: { tip: Tip }) {
   const headerImgClass =
     props.tip.img === null ? "powertip-header-noimg" : "powertip-header-wimg";
-  const additionalStyles: CSSProperties = {
-    left: props.offset.x,
-    top: props.offset.y,
-  };
 
   return (
-    <div className={"powertip"} style={additionalStyles}>
+    <div className={"powertip"}>
       <div className={"powertip-header outline-black " + headerImgClass}>
         {props.tip.header}
       </div>
@@ -59,6 +62,7 @@ export function Hitbox(props: {
   if (props.magGlass) {
     classes += " mag-glass";
   }
+  classes += " outline";
   const style: CSSProperties = {
     left: props.hitbox.x,
     top: props.hitbox.y,
@@ -66,16 +70,13 @@ export function Hitbox(props: {
     height: props.hitbox.h,
     zIndex: props.hitbox.z,
   };
-  const [shouldShow, setShouldShow] = useState(false);
+  const tooltipID = useId();
 
   return (
-    <div
-      className={classes}
-      style={style}
-      onMouseEnter={() => setShouldShow(true)}
-      onMouseLeave={() => setShouldShow(false)}
-    >
-      {shouldShow && props.children}
+    <div className={classes} style={style} data-tooltip-id={tooltipID}>
+      <Tooltip style={{ background: "transparent" }} id={tooltipID}>
+        {props.children}
+      </Tooltip>
     </div>
   );
 }
@@ -86,33 +87,15 @@ export function PowerTipBlock(props: {
   tips: Tip[];
   offset: { x: string; y: string };
 }) {
-  const chunkSize = 4;
-  const chunks: Tip[][] = [];
-  for (let i = 0; i < props.tips.length; i += chunkSize) {
-    const chunk = props.tips.slice(i, i + chunkSize);
-    chunks.push(chunk);
-  }
-
   return (
     <Hitbox magGlass={props.magGlass} hitbox={props.hitbox}>
-      {chunks.map((chunk, i) => {
-        return (
-          <div
-            key={"power-tip-block-chunk-" + i}
-            className={"powertip-multicol-column"}
-          >
-            {chunk.map((tip, j) => {
-              return (
-                <PowerTip
-                  key={"power-tip-block-item-" + i + "-" + j}
-                  tip={tip}
-                  offset={props.offset}
-                />
-              );
-            })}
-          </div>
-        );
-      })}
+      <div
+        className={"flex max-h-120 flex-col flex-wrap justify-start gap-0.5"}
+      >
+        {props.tips.map((tip: Tip, i: number) => {
+          return <PowerTip key={"power-tip-block-item-" + i} tip={tip} />;
+        })}
+      </div>
     </Hitbox>
   );
 }
@@ -121,18 +104,11 @@ export function PowerTipStrip(props: {
   magGlass: boolean;
   hitbox: HitBox;
   tips: Tip[];
-  offset: { x: string; y: string };
 }) {
   return (
     <Hitbox magGlass={props.magGlass} hitbox={props.hitbox}>
       {props.tips.map((tip, i) => {
-        return (
-          <PowerTip
-            key={"power-tip-strip-" + i}
-            tip={tip}
-            offset={props.offset}
-          />
-        );
+        return <PowerTip key={"power-tip-strip-" + i} tip={tip} />;
       })}
     </Hitbox>
   );
