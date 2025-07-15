0fe653c44465df2ca6db2bfac0187fc203abb090
Peijun Ma <peijun.ma@protonmail.com>
2025-07-15 11:09:08 -0400

    Add prettier

--- slay-the-relics-extension/src/components/Relic/Relic.tsx
+++ slay-the-relics-extension/src/components/Relic/Relic.tsx
@@ -1,57 +1,65 @@
-import {HitBox, PowerTipStrip, Tip} from "../Tip/Tip";
+import { HitBox, PowerTipStrip, Tip } from "../Tip/Tip";
 
-const RELIC_HITBOX_WIDTH = 3.75 //%
-const RELIC_HITBOX_LEFT = 1.458 //%
-const RELIC_HITBOX_MULTIPAGE_OFFSET = 1.875 //%
+const RELIC_HITBOX_WIDTH = 3.75; //%
+const RELIC_HITBOX_LEFT = 1.458; //%
+const RELIC_HITBOX_MULTIPAGE_OFFSET = 1.875; //%
 
 export class RelicProp {
-    name: string;
-    description: string;
-    additionalTips: Tip[]
+  name: string;
+  description: string;
+  additionalTips: Tip[];
 
-    constructor(name: string, description: string, additionalTips: Tip[]) {
-        this.name = name;
-        this.description = description;
-        this.additionalTips = additionalTips;
-    }
+  constructor(name: string, description: string, additionalTips: Tip[]) {
+    this.name = name;
+    this.description = description;
+    this.additionalTips = additionalTips;
+  }
 
-    getTips(): Tip[] {
-        return [new Tip(this.name, this.description, null)].concat(this.additionalTips);
-    }
+  getTips(): Tip[] {
+    return [new Tip(this.name, this.description, null)].concat(
+      this.additionalTips,
+    );
+  }
 }
 
 export function LookupRelic(relic: string): RelicProp {
-    switch (relic) {
-        case "akabeko":
-            return new RelicProp("Akabeko", "mu", [])
-        default:
-            return new RelicProp(relic, relic, []);
-    }
+  switch (relic) {
+    case "akabeko":
+      return new RelicProp("Akabeko", "mu", []);
+    default:
+      return new RelicProp(relic, relic, []);
+  }
 }
 
-export function Relic(props: { relic: RelicProp, hitbox: HitBox }) {
-    return (
-        <PowerTipStrip magGlass={true} hitbox={props.hitbox} tips={props.relic.getTips()}
-                       offset={{x: "70%", y: "-30%"}}/>
-    )
+export function Relic(props: { relic: RelicProp; hitbox: HitBox }) {
+  return (
+    <PowerTipStrip
+      magGlass={true}
+      hitbox={props.hitbox}
+      tips={props.relic.getTips()}
+      offset={{ x: "70%", y: "-30%" }}
+    />
+  );
 }
 
-export function RelicBar(props: { relics: RelicProp[], multiPage: boolean }) {
-    const multiPage = props.multiPage ? 1 : 0;
-    return (
-        <div id={"relic-bar"}>
-            {props.relics.map((relic, i) => {
-                const hitbox = {
-                    x: (RELIC_HITBOX_LEFT + i * RELIC_HITBOX_WIDTH + multiPage * RELIC_HITBOX_MULTIPAGE_OFFSET) + '%',
-                    y: 6.111 + '%',
-                    z: 1,
-                    w: 3.75 + '%',
-                    h: 8.666 + '%'
-                }
-                return (
-                    <Relic key={"relic-bar-" + i} hitbox={hitbox} relic={relic}/>
-                )
-            })}
-        </div>
-    )
+export function RelicBar(props: { relics: RelicProp[]; multiPage: boolean }) {
+  const multiPage = props.multiPage ? 1 : 0;
+  return (
+    <div id={"relic-bar"}>
+      {props.relics.map((relic, i) => {
+        const hitbox = {
+          x:
+            RELIC_HITBOX_LEFT +
+            i * RELIC_HITBOX_WIDTH +
+            multiPage * RELIC_HITBOX_MULTIPAGE_OFFSET +
+            "%",
+          y: 6.111 + "%",
+          z: 1,
+          w: 3.75 + "%",
+          h: 8.666 + "%",
+        };
+        return <Relic key={"relic-bar-" + i} hitbox={hitbox} relic={relic} />;
+      })}
+    </div>
+  );
 }
